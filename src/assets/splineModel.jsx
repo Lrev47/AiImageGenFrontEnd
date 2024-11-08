a20905e4f0e2ddcd05ae4d172a967b5e80894aa4
luis revilla <lrev4766@gmail.com>
2024-11-08 12:02:01 -0600

    Add src/assets/splineModel.jsx

---
+++ src/assets/splineModel.jsx
@@ -0,0 +1,11 @@
+// SplineModel.jsx
+import React from "react";
+import Spline from "@splinetool/react-spline";
+
+function SplineModel() {
+  return (
+    <Spline scene="https://prod.spline.design/rcYX4xebxiNAmwBp/scene.splinecode" />
+  );
+}
+
+export default SplineModel;
