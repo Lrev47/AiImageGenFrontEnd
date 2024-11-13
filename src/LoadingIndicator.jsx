eb74ae87a1b1759c4a35afbc87e49b91db583690
luis revilla <lrev4766@gmail.com>
2024-11-12 21:07:30 -0600

    update dependencies

--- src/LoadingIndicator.jsx
+++
@@ -1,8 +0,0 @@
-// LoadingIndicator.jsx
-import React from "react";
-
-const LoadingIndicator = () => {
-  return <div className="loading-indicator">Loading...</div>;
-};
-
-export default LoadingIndicator;
