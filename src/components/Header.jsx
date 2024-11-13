41bcfbc706183a26866c867ae87c781611cf1da0
luis revilla <lrev4766@gmail.com>
2024-11-12 21:07:09 -0600

    Add Header.jsx in components

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+// Header.jsx
+import React from "react";
+import { Link } from "react-router-dom";
+import "../style/Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-logo">
+        <h1 className="website-title">MetaCre8</h1>{" "}
+        {/* Updated website name with gradient text */}
+      </div>
+      <nav className="header-nav">
+        <Link to="/login" className="nav-button">
+          Login
+        </Link>
+        <Link to="/try-for-free" className="nav-button try-button">
+          Try For Free
+        </Link>
+        <div className="menu-icon">
+          <div className="menu-bar"></div>
+          <div className="menu-bar"></div>
+          <div className="menu-bar"></div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
