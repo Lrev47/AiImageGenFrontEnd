8f72ebf0c7b6eb4f8480846505af64ac33052760
luis revilla <lrev4766@gmail.com>
2024-11-18 23:30:29 -0600

    Modify GeneratePage.jsx

--- src/components/GeneratePage.jsx
+++ src/components/GeneratePage.jsx
@@ -1,9 +1,13 @@
-// GeneratePage.jsx
+// src/components/GeneratePage.jsx
 import React from "react";
 import { Link } from "react-router-dom";
 import "../style/GeneratePage.css";
 import WorkflowTile from "./WorkflowTile";
 
+/**
+ * Renders the Generate Page with a grid of workflow options.
+ * @returns {JSX.Element} - The Generate Page component.
+ */
 const GeneratePage = () => {
   return (
     <div className="generate-page">
@@ -13,32 +17,32 @@ const GeneratePage = () => {
           title="MidJourney V6.1"
           description="Vibrant, colorful, and abstract art."
           image="https://imagizer.imageshack.com/img923/5844/AumCP9.png"
-          link="/workflow/fluxdev1"
+          link="/workflows/midJourneyV6.1"
         />
         <WorkflowTile
           title="Retro Hand Drawn Movie Posters"
           description="Generate stunning AI-powered movie posters."
           image="https://imagizer.imageshack.com/img922/5329/T8F4aL.png"
-          link="/workflow/movie-poster"
+          link="/workflows/movie-poster"
         />
         <WorkflowTile
           title="Retro Anime"
           description="Generate Retro 90s style anime characters."
           image="https://imagizer.imageshack.com/img923/9671/dos2f4.png"
-          link="/workflow/RetroAnime"
+          link="/workflows/RetroAnime"
           className="retro-anime"
         />
         <WorkflowTile
           title="Galactixy Illustrations"
           description="Folk art with a mystical touch."
           image="https://imagizer.imageshack.com/img922/8950/apStVx.png"
-          link="/workflow/GalactixyIllustrations"
+          link="/workflows/GalactixyIllustrations"
         />
         <WorkflowTile
           title="Candid Photos"
           description="Slice of life photography."
           image="https://imagizer.imageshack.com/img923/7379/CUvNGU.png"
-          link="/workflow/CandidPhotos"
+          link="/workflows/CandidPhotos"
         />
       </div>
     </div>
