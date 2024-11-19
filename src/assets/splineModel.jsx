2da6b5788cf5f73cdcc1cdba0092fbf4d223a7a0
luis revilla <lrev4766@gmail.com>
2024-11-18 23:30:29 -0600

    Modify splineModel.jsx

--- src/assets/splineModel.jsx
+++ src/assets/splineModel.jsx
@@ -1,7 +1,11 @@
-// SplineModel.jsx
+// src/assets/splineModel.jsx
 import React from "react";
 import Spline from "@splinetool/react-spline";
 
+/**
+ * Renders a Spline 3D model.
+ * @returns {JSX.Element} - The Spline component.
+ */
 function SplineModel() {
   return (
     <Spline scene="https://prod.spline.design/rcYX4xebxiNAmwBp/scene.splinecode" />
