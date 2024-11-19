97dd42083d71eb52cbc2f32b0ae3536d3e46d40e
luis revilla <lrev4766@gmail.com>
2024-11-18 23:30:29 -0600

    Modify WorkflowTile.jsx

--- src/components/WorkflowTile.jsx
+++ src/components/WorkflowTile.jsx
@@ -1,11 +1,21 @@
-// WorkflowTile.jsx
+// src/components/WorkflowTile.jsx
 import React from "react";
 import { Link } from "react-router-dom";
 import "../style/WorkflowTile.css";
 
-const WorkflowTile = ({ title, description, image, link, style }) => {
+/**
+ * Renders a tile representing a workflow option.
+ * @param {Object} props - The component props.
+ * @param {string} props.title - The title of the workflow.
+ * @param {string} props.description - A brief description of the workflow.
+ * @param {string} props.image - The background image URL for the tile.
+ * @param {string} props.link - The route to navigate to when the tile is clicked.
+ * @param {string} [props.className] - Additional CSS classes for custom styling.
+ * @returns {JSX.Element} - The WorkflowTile component.
+ */
+const WorkflowTile = ({ title, description, image, link, className }) => {
   return (
-    <Link to={link} className="workflow-tile">
+    <Link to={link} className={`workflow-tile ${className || ""}`}>
       <div
         className="tile-image"
         style={{ backgroundImage: `url(${image})` }}
